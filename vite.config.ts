1fd96e4fefbda03cf6e6d64249d361acd3912274
Ivan Vitiaev <godofluck89@gmail.com>
2026-08-25 18:36:50 +0000

    add font preload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import reactSsg from 'vite-plugin-react-ssg'
+import { ViteWebfontDownload } from 'vite-plugin-webfont-dl';
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), tailwindcss(), reactSsg()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    ViteWebfontDownload(),
+    reactSsg()
+  ],
 })
